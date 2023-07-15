36a7a421065b6cf7963b9510fab3cbc00b931022
NehaGautam31 <137690749+NehaGautam31@users.noreply.github.com>
2023-07-15 15:35:15 +0530

    nehaaa

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,5 +1,8 @@
 
-let firebaseConfig = {
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-app.js";
+  
+  let firebaseConfig = {
     apiKey: "AIzaSyDnPg9axnyHM9y99XcxlE8YwfJaahuHYII",
     authDomain: "fashion-1b9ff.firebaseapp.com",
     projectId: "fashion-1b9ff",
@@ -8,7 +11,10 @@ let firebaseConfig = {
     appId: "1:397503157967:web:0c3f32efeed777ef1709ba"
   };
 
-// Initialize Firebase
+  // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
+
+
+
 
 let db = firebase.firestore();
